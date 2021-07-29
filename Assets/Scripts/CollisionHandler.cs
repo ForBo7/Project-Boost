932f3afab91cb1830f3de7c356ade709f00e3a78
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-29 21:40:30 +0500

    Proceeding levels can noww load. Last level loops back to the first level.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+// Handles the collisions of the rocket and loads scenes.
 public class CollisionHandler : MonoBehaviour
 {
+    // Get the current scene's index.
+    private int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
     // Handles the collisions of the rocket.
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Finish"))
         {
-            Debug.Log("Collided with the finish pad!");
+            LoadNextLevel();
         }
         else if (collision.gameObject.CompareTag("Friendly"))
         {
@@ -22,9 +32,20 @@ public class CollisionHandler : MonoBehaviour
     }
 
     // Get the current scene's index and reload the scene.
-    private static void ReloadScene()
+    private void LoadNextLevel()
     {
-        int currentSceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
-        UnityEngine.SceneManagement.SceneManager.LoadScene(currentSceneIndex);
+        int nextSceneIndex = currentSceneIndex + 1;
+        // Loop back to first level if at last level.
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    // Get the current scene's index and reload the scene.
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
